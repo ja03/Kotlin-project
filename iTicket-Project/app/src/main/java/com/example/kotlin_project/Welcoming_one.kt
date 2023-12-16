b7c1e633e89173cd7c0c61ee7bc7615a37a3d972
Khalid Dabbour <khaliddabbour03@gmail.com>
2023-12-17 00:53:05 +0300

    linking between welcoming and log in

--- iTicket-Project/app/src/main/java/com/example/kotlin_project/Welcoming_one.kt
+++ iTicket-Project/app/src/main/java/com/example/kotlin_project/Welcoming_one.kt
@@ -1,12 +1,23 @@
 package com.example.kotlin_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button  // Import the Button class
 
 class Welcoming_one : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcoming_one)
 
+        // Fix the typo in the button variable name
+        val learnMoreBtn = findViewById<Button>(R.id.learn_more_btn)
+
+        // Fix the typo in the intent variable name
+        learnMoreBtn.setOnClickListener{
+            val learnMoreIntent = Intent(this, Log_in::class.java)
+            startActivity(learnMoreIntent)
+        }
     }
 }
