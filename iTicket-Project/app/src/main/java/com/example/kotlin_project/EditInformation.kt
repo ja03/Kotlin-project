71059c28ec4632020ba7ed9259fb6db800214630
Daniel Shaqfeh <danielshaqfeh@gmail.com>
2023-12-16 21:40:13 +0300

    add new pages

---
+++ iTicket-Project/app/src/main/java/com/example/kotlin_project/EditInformation.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditInformation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.edit_profile)
+    }
+}
