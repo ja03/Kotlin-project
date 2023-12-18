74e750fd39ee8c40d5c2e0165a29eb4eba3f68c5
Daniel Shaqfeh <danielshaqfeh@gmail.com>
2023-12-18 23:15:19 +0300

    changed create ticket btn

--- iTicket-Project/app/src/main/java/com/example/kotlin_project/filter_tickets.kt
+++ iTicket-Project/app/src/main/java/com/example/kotlin_project/filter_tickets.kt
@@ -14,7 +14,6 @@ class filter_tickets : AppCompatActivity() {
         setContentView(R.layout.activity_filter_tickets)
 
         val filterBtn = findViewById<Button>(R.id.filter_btn)
-
         filterBtn.setOnClickListener{
             val filterBtn = Intent(this, Tickets::class.java)
             startActivity(filterBtn)
@@ -24,33 +23,29 @@ class filter_tickets : AppCompatActivity() {
             val popBtn = Intent(this, Tickets::class.java)
             startActivity(popBtn)
         }
-
-
+        //-------------------------
         var spinnerSortClient:Spinner = findViewById(R.id.spinnerSortClient)
         var adapter01: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
             this,
             R.array.sort_items,
             android.R.layout.simple_spinner_dropdown_item
         )
-        adapter01.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinnerSortClient.adapter = adapter01
-
+        //*************************
         var spinnerSortEmployees:Spinner = findViewById(R.id.spinnerSortEmployees)
         var adapter02: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
             this,
             R.array.sort_items_emp,
             android.R.layout.simple_spinner_dropdown_item
         )
-        adapter02.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinnerSortEmployees.adapter = adapter02
-
+        //***********************
         var spinnerSortState:Spinner = findViewById(R.id.spinnerSortState)
         var adapter03: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
             this,
             R.array.sort_items_state,
             android.R.layout.simple_spinner_dropdown_item
         )
-        adapter03.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinnerSortState.adapter = adapter03
     }
 }
